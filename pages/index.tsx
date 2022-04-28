4999a06d11372a4e7a8ad2e0471ff583bc82310f
JinhaHwang <jack.eyes.00@gmail.com>
2022-04-28 14:18:43 +0900

    feature: add hls player sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import VideoPlayer from '@/components/VideoPlayer'
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
-  return <div className="flex flex-col bg-red-500 text-3xl font-bold underline">hello</div>
+const Home: NextPage<{ data: any }> = ({
+  data = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+}) => {
+  console.log(data)
+  return (
+    <div className="flex max-w-lg flex-wrap">
+      <VideoPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+      <VideoPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+      <VideoPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+      <VideoPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
+    </div>
+  )
 }
 
 export default Home
